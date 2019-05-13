ec11966875fec635c7f0875c5e21fc3f91cbb49c
Oscar Marcelo <oscarmarcelo@users.noreply.github.com>
2019-05-13 18:02:01 +0100

    Remove unused command alias

--- Terminal/aliases.bash
+++ Terminal/aliases.bash
@@ -2,9 +2,6 @@
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
 
-# Shortcuts
-alias server="cd ~/Dropbox/server"
-
 # Disable & Enable GateKeeper
 alias openGate="sudo spctl --master-disable"
 alias closeGate="sudo spctl --master-enable"
